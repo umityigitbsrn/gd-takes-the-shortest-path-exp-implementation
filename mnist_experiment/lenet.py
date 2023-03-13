8d1be3a773f61a14b8fc496fbca37faf22f6698e
umityigitbsrn <umityigitbsrn@gmail.com>
2023-03-13 02:22:12 -0700

    initialization of the repo

--- mnist_experiment/lenet.py
+++ mnist_experiment/lenet.py
@@ -8,6 +8,22 @@ class LeNet(nn.Module):
 
         self.num_classes = num_classes
 
+        # self.conv_layer_01 = nn.Sequential(
+        #     nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, padding=2, bias=False),
+        #     nn.Sigmoid(),
+        #     nn.AvgPool2d(kernel_size=2, stride=2)
+        # )
+        #
+        # self.conv_layer_02 = nn.Sequential(
+        #     nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, bias=False),
+        #     nn.Sigmoid(),
+        #     nn.AvgPool2d(kernel_size=2, stride=2)
+        # )
+        #
+        # self.fc_01 = nn.Linear(in_features=400, out_features=120, bias=False)
+        # self.fc_02 = nn.Linear(in_features=120, out_features=84, bias=False)
+        # self.fc_03 = nn.Linear(in_features=84, out_features=self.num_classes, bias=False)
+
         self.conv_layer_01 = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, padding=2),
             nn.Sigmoid(),
@@ -28,7 +44,7 @@ class LeNet(nn.Module):
         x = self.conv_layer_01(x)
         x = self.conv_layer_02(x)
 
-        x = reshape(x, (-1,))
+        x = reshape(x, (x.shape[0], -1))
         x = self.fc_01(x)
         x = self.fc_02(x)
         out = self.fc_03(x)
--- utils.py
+++ utils.py
@@ -1,14 +1,37 @@
 import torch
+from typing import Dict
 
 
 def normalized_misfit(labels: torch.Tensor, data: torch.Tensor, init_model: torch.nn.Module, model: torch.nn.Module) \
-        -> torch.Tensor:
+        -> float:
     init_model.eval()
     model.eval()
-    misfit = torch.linalg.vector_norm((labels - model(data)), ord=2, dim=1)
-    init_misfit = torch.linalg.vector_norm((labels - init_model(data)), ord=2, dim=1)
-    return torch.div(misfit, init_misfit)
+
+    # label transformation
+    transformed_labels = labels.nonzero()[:, 1]
+
+    predictions = model(data)
+    init_predictions = init_model(data)
+
+    transformed_predictions = torch.argmax(predictions, dim=1)
+    transformed_init_predictions = torch.argmax(init_predictions, dim=1)
+
+    misfit = torch.linalg.vector_norm((transformed_labels - transformed_predictions).type(torch.FloatTensor), ord=2)
+    init_misfit = torch.linalg.vector_norm((transformed_labels - transformed_init_predictions).type(torch.FloatTensor)
+                                           , ord=2)
+    return (misfit / init_misfit).item()
 
 
-def normalized_distance(init_weight, weight):
-    pass
+def normalized_distance(init_model: torch.nn.Module, model: torch.nn.Module) -> Dict[str, float]:
+    init_model.eval()
+    model.eval()
+
+    normalized_distance_dict = {}
+
+    for ((init_name, init_param), (name, param)) in zip(init_model.named_parameters(), model.named_parameters()):
+        if init_name.find('weight') != -1:
+            distance = torch.linalg.norm((param - init_param).reshape((param.shape[0], -1)), ord='fro')
+            init_distance = torch.linalg.norm(init_param.reshape((init_param.shape[0], -1)), ord='fro')
+            normalized_distance_dict[init_name] = (distance / init_distance).item()
+
+    return normalized_distance_dict
